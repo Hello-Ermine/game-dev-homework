9ff771565bf13225d311dd23ddeb24bb7fb4aff6
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2021-11-02 13:38:11 +0700

    Edit02
    ตอนนี้ตัวละครจะติดดินละเดินแค่ A D และล่องหน space

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,7 @@
 import Phaser from "phaser";
 
 let bg, player;
-let keyW, keyA, keyS, keyD, keyAtk;
+let keyA, keyD, keyAtk;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -36,9 +36,7 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         })
         //KEY
-        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyAtk = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         //wallblock
@@ -49,16 +47,11 @@ class GameScene extends Phaser.Scene {
         bg.tilePositionX += 2;
         player.anims.play('playerrun', true);
 
-        if (keyW.isDown) {
-            player.setVelocityY(-200);
-        } else if (keyS.isDown) {
-            player.setVelocityY(200);
-        } else {
-            player.setVelocityY(0);
-        } if (keyA.isDown) {
-            player.setVelocityX(-200);
+        
+        if (keyA.isDown) {
+            player.setVelocityX(-250);
         } else if (keyD.isDown) {
-            player.setVelocityX(200);
+            player.setVelocityX(250);
         } else {
             player.setVelocityX(0);
         } if (keyAtk.isDown) {
