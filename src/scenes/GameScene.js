9bfdab5e17d62b7766306b7ecab296ba526b82f7
panithee <panithee.pnt@mail.kmutt.ac.th>
2021-11-02 13:28:07 +0700

    test

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 
-
+let fox;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -10,16 +10,30 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.spritesheet('fox', 'src/image/fox/playerFox.png',
+                                { frameWidth: 500, frameHeight: 300 });
 
     }
 
     create() {
+        fox = this.physic.add.sprite(200, 500, 'fox').setScale(0.5).setOffset(0, 0);
+
+        this.anims.create({
+            key: 'foxAni',
+            frames: this.anims.generateFrameNumbers('fox',{
+                start: 0,
+                end: 7
+            }),
+            duration: 500,
+            repeat: -1
+        })
+        
+        fox.setCollideWorldBounds(true)
         
     }
 
     update(delta, time) {
-        
+        fox.anims.play('foxAni', true);
     }
 }
 export default GameScene;
