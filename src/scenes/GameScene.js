6627513b3626bbf08e88ecf53d5ea597f5f0d472
Nannapatx <nannapat.it@mail.kmutt.ac.th>
2021-11-03 00:01:16 +0700

    Update 3/11/2021

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,12 @@
 import Phaser from "phaser";
 
 let background;
-let wolf;
+let rabbit;
+let keyW;
+let keyA;
+let keyS;
+let keyD;
+
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -12,28 +17,60 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('bg','src/images/background-game-png-2.png');
-        this.load.image('wolf','src/images/wolf.png',{ frameWidth : 160, framHeight : 799});
-
+        this.load.spritesheet('rabbit','src/images/rabbitwalkinganimation.png',{ frameWidth : 197.5, frameHeight : 300});
+    
     }
 
     create() {
-        //background = this.add.tileSprite(0, 0, 450, 640, 'bg').setOrigin(0, 0);
-        wolf = this.physics.add.sprite(100, 500, 'wolf').setScale(0.5).setDepth(99);
+        background = this.add.tileSprite(0, 0, 450, 640, 'bg').setOrigin(0, 0);
+        rabbit = this.add.sprite(100, 500, 'rabbit').setScale(0.5).setDepth(99);
         this.anims.create({
-            key: 'wolfAni',
-            frames: this.anims.generateFrameNumbers('wolf', {
+            key: 'rabbitAni',
+            frames: this.anims.generateFrameNumbers('rabbit', {
                 start: 0,
-                end: 4
+                end: 3
             }),
             duration: 1000,
             framerate: 0,
             repeat: -1
         })
-        wolf.anims.play('wolfAni', true)
+        
+        rabbit.anims.play('rabbitAni', true);
+        // rabbit.setCollideWorldBounds(true)
+        // keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        // keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        // keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        // keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update(delta, time) {
-        //background.tilePositionX += 2;
+        // if (keyW.isDown) {
+
+        //     rabbit.setVelocityY(-500);
+
+        // } else if (keyS.isDown) {
+
+        //     rabbit.setVelocityY(500);
+
+        // } else {
+
+        //     rabbit.setVelocityY(0);
+
+        // }
+
+        // if (keyA.isDown) {
+
+        //     rabbit.setVelocityX(-500);
+
+        // } else if (keyD.isDown) {
+
+        //     rabbit.setVelocityX(500);
+
+        // } else {
+
+        //     rabbit.setVelocityX(0);
+        // }
+        background.tilePositionX += 2;
     }
 }
 export default GameScene;
