072188dd3417a30240872768315da580fdeb1a7e
Nannapatx <nannapat.it@mail.kmutt.ac.th>
2021-11-02 23:12:00 +0700

    Update 2/11/21

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 
-
+let background;
+let wolf;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -10,16 +11,29 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('bg','src/images/background-game-png-2.png');
+        this.load.image('wolf','src/images/wolf.png',{ framewidth : 225, framheight : 400});
 
     }
 
     create() {
-        
+        //background = this.add.tileSprite(0, 0, 450, 640, 'bg').setOrigin(0, 0);
+        wolf = this.physics.add.sprite(100, 500, 'wolf').setScale(0.5).setDepth(99);
+        this.anims.create({
+            key: 'wolfAni',
+            frames: this.anims.generateFrameNumbers('wolf', {
+                start: 0,
+                end: 9
+            }),
+            duration: 1000,
+            framerate: 0,
+            repeat: -1
+        })
+        wolf.anims.play('wolfAni', true)
     }
 
     update(delta, time) {
-        
+        //background.tilePositionX += 2;
     }
 }
 export default GameScene;
