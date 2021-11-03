7618a6392f8043d54543029e9783faed1e818ead
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2021-11-03 17:43:07 +0700

    E05 Sound
    ใส่ซาวไงละลองกด SPACE ดูสิเจ้าพวกโง่วววว

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,9 @@
 import Phaser from "phaser";
+import { NONE } from "phaser/src/const";
 
 let mainbg, player;
 let keyA, keyD, keyVis;
+let music,soundVis;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -13,6 +15,8 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('mainbg', 'src/image/BackG.jpg');
         this.load.spritesheet('player', 'src/image/player1.png', { frameWidth: 180, frameHeight: 247 });
+        this.load.audio('song','src/image/song/gamesong.mp3');
+        this.load.audio('soundVis','src/image/song/visiblesound.mp3');
     }
 
     create() {
@@ -43,10 +47,15 @@ class GameScene extends Phaser.Scene {
         keyVis = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         //wallblock
         player.setCollideWorldBounds(true);
+        //music
+        music = this.sound.add('song')
+            .setVolume(0.6)
+            .play({loop: true});
+        soundVis = this.sound.add('soundVis');
     }
 
     update(delta, time) {
-        mainbg.tilePositionX += 3;
+        mainbg.tilePositionX += 4;
         //Key A D STOP
         if (keyA.isDown) {player.setVelocityX(-200);}
         else if (keyD.isDown) {player.setVelocityX(300);}
@@ -58,8 +67,12 @@ class GameScene extends Phaser.Scene {
         else {player.anims.play('playerrun', false);}
         
         //Visible
-        if (keyVis.isDown) {player.setVisible();}
-        else if (keyVis.isUp) {player.setVisible(1);}
+        if (keyVis.isDown) {
+            player.setVisible()
+        } else if (keyVis.isUp) {
+            player.setVisible(1)
+            soundVis.play();
+        } 
     }
 }
 export default GameScene;
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 
 let buttonPlay;
 let bg;
+let mainmusic;
 
 class MainMenu extends Phaser.Scene {
     constructor(test) {
@@ -13,9 +14,11 @@ class MainMenu extends Phaser.Scene {
     preload() {
         this.load.image('bg', 'src/image/MainBG.jpg');
         this.load.image('play', 'src/image/start.png');
+        this.load.audio('mainmusic','src/image/song/mainsong.mp3');
     }
 
     create() {
+        //mainmusic = this.sound.add('mainmusic').play();
         //mainBG
         bg = this.add.tileSprite(0, 0, 1360, 340, 'bg')
             .setScale(2)
@@ -25,8 +28,10 @@ class MainMenu extends Phaser.Scene {
         buttonPlay = this.add.image(680,340, 'play').setScale(1.5);
         buttonPlay.setInteractive();
         buttonPlay.on('pointerup', ()=>{
-            this.scene.start('GameScene');
+            this.scene.start('GameScene')
         })
+
+        
     }
 
     update(delta, time) {
