dfb9e712d0d9c2d679d51c3c7ee166d203712f81
thannichasompan <thannicha.som@mail.kmutt.ac.th>
2021-11-05 16:16:23 +0700

    bg

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 
-
+let bg;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -10,15 +10,35 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
+
+
+        this.load.image('bg', 'src/image/Bg.jpeg');
+
+       // this.load.spritesheet('bird','src/image/bird.png',
+        //{ frameWidth: 410 , frameHeight: 310});
         
 
     }
 
     create() {
+
+        background = this.add.tileSprite(0, 0, 850, 1400, 'bg').setOrigin(0, 0);
+
+
+    //animation
+        //this.anims.create({
+          //  key: 'birdAni',
+         //   frames: this.anims.generateFrameNumbers('bird', {
+         //       start: 0,
+         //       end: 7
+       //     }),
+       //     duration: 500,    
+       //     repeat: -1
         
     }
 
     update(delta, time) {
+        background.tilePositionY -= 10;
         
     }
 }
