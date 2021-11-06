c9a57a57ce3b2e05a554cf1b82e3b1537a4e5f04
JoNoteJoMaMa <notnot45@hotmail.com>
2021-11-06 11:48:01 +0700

    ARkkk

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 
 let menu;
+let startbutton;
 
 class MainMenu extends Phaser.Scene {
     constructor(test) {
@@ -13,7 +14,14 @@ class MainMenu extends Phaser.Scene {
 
         this.load.spritesheet('menu','image/Menunaja.png',
         { frameWidth: 680 , frameHeight: 553});
+
+        this.load.image('delta','image/delta.png');
+
+        this.load.image('rush','image/rush.png');
+
+        this.load.image('soul','image/yellow heart.png');
         
+        this.load.image('start','image/bigshot.png');
 
     }
 
@@ -30,7 +38,18 @@ class MainMenu extends Phaser.Scene {
             duration: 500,    
             repeat: -1
         })
-        
+
+        this.add.image(200,250,'delta').setScale(0.7);
+
+        this.add.image(600,250,'rush').setScale(0.7);
+
+        this.add.image(395,250,'soul').setScale(0.1);
+
+        startbutton = this.add.image(395,400,'start').setScale(0.75);
+        soundButton.setInteractive();
+        soundButton.on('pointerup',()=>{
+            
+        });
     
 
         
