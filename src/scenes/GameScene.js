95a1509677663e98ac774eb08f57fcf1c18d6616
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2021-11-06 11:58:53 +0700

    Update2

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -29,6 +29,11 @@ class GameScene extends Phaser.Scene {
         tenis = this.physics.add.sprite(225, 500, 'tenis')
             .setScale(0.45)
             .setCollideWorldBounds(true);
+            bg.tilePositionX -= 10;
+            tenis = this.physics.add.sprite(150,520,'tenis').setScale(0.7).setDepth(10).setGravityY(900);;
+           
+            bg.tilePositionY -= 10;
+             goodfrog.anims.play('tenisAni', true);    
 
             
 
@@ -43,27 +48,27 @@ class GameScene extends Phaser.Scene {
 
     update(delta, time) {
         bg.tilePositionX -= 10;
-        tenis = this.physics.add.sprite(150,520,'tenis').setScale(0.7).setDepth(10).setGravityY(1000);;
+        tenis = this.physics.add.sprite(150,520,'tenis').setScale(0.7).setDepth(10).setGravityY(900);;
        
         bg.tilePositionY -= 10;
-        // goodfrog.anims.play('frogAni', true);
+         goodfrog.anims.play('tenisAni', true);
 
         tp.anims.play('tpAni', true);
 
-        // if (keyW.isDown) {
-        //     goodfrog.setVelocityY(-500);
-        // } else if (keyS.isDown) {
-        //     goodfrog.setVelocityY(500);
-        // } else {
-        //     goodfrog.setVelocityY(0);
-        // }
-        // if (keyA.isDown) {
-        //     goodfrog.setVelocityX(-500);
-        // } else if (keyD.isDown) {
-        //     goodfrog.setVelocityX(500);
-        // } else {
-        //     goodfrog.setVelocityX(0);
-        // }
+        if (keyW.isDown) {
+            tenis.setVelocityY(-500);
+        } else if (keyS.isDown) {
+           tenis.setVelocityY(500);
+        } else {
+           tenis.setVelocityY(0);
+        }
+        if (keyA.isDown) {
+           tenis.setVelocityX(-500);
+        } else if (keyD.isDown) {
+           tenis.setVelocityX(500);
+        } else {
+           tenis.setVelocityX(0);
+        }
 
     
         
@@ -71,11 +76,7 @@ class GameScene extends Phaser.Scene {
       
         
         
-        tenis.setCollideWorldBounds(true);
-
-        bg = this.sound.add('bg').setVolume(0.2);
-
-        theme.play({loop: true});
+        
         
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
@@ -83,53 +84,7 @@ class GameScene extends Phaser.Scene {
         keySb = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
-    update(delta, time) {
     
-        bg.tilePositionY -= 10;
-        
-        if (keyX.isDown) {
-            sparkle.play({loop: false});
-            queen.anims.play('queenSleep', true,)
-        
-        }
-        else if (keySb.isDown) {
-           
-            jump.play({loop: false});
-            queen.anims.play('queenJump', true,)
-            queen.setVelocityY(-100);
-        
-        }
-        else if (keyCtrl.isDown) {
-            bloop.play({loop: false});
-            queen.anims.play('queenSlide', true,)
-            
-            
-        }
-        else if (keyA.isDown) {
-            bg.tilePositionX -= 2;
-            queen.setVelocityX(-200);
-            queen.anims.play('queenDash', true);
-            queen.flipX=true;
-            
-        }  else if (keyD.isDown) {
-            bg.tilePositionX += 2;
-            queen.setVelocityX(200);
-            queen.anims.play('queenDash', true);
-            queen.flipX=false;
-            
-        } 
-        else {
-            queen.setVelocityX(0);
-           
-           queen.anims.play('queenAni2', true);
-           
-        }
-        
-        
-        cloud1.tilePositionX -= 1;
-        cloud2.tilePositionX -= 1;
-        snow.tilePositionY -= 1;
-    }
 }
 export default GameScene;
 
